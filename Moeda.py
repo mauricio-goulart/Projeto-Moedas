b1c7cdee61ccf9a287ed715aa56ddcd9488906e0
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-01 10:36:51 -0300

    Update Moeda.py

--- Moeda.py
+++ Moeda.py
@@ -11,20 +11,26 @@ def pegar_cotacoes():
 
     cotacao_dolar = requisao_dic['USDBRL']['bid']
 
-    print(cotacao_dolar)
+    texto = f'Dolar:$[{cotacao_dolar}]'
 
-pegar_cotacoes()
+    texto_cotacao["text"] = texto
+
 
 #interface grifica
 
 janela = Tk()
+janela.geometry('380x200')
+
 
 janela.title('Casa da Moeda')
 texto_inicio = Label(janela, text='Bem-Vindo ao cotador de moedas.Clique no botão para ver.')
-texto_inicio.grid(column=0, row=0)
+texto_inicio.grid(column=0, row=0, padx=25, pady=15)
 
 botao = Button(janela, text='Buscar Cotação', command=pegar_cotacoes)
-botao.grid(column=0, row=1)
+botao.grid(column=0, row=1, padx=25, pady=15)
+
+texto_cotacao = Label(janela, text="")
+texto_cotacao.grid(column=0, row=3, padx=25,pady=10)
 
 
 
